58ed9b8ceb4ea46498cb585061d5ebbf038f7551
davis245 <davis_franklin@outlook.com>
2026-02-19 20:57:11 -0800

    Remove unnecessary prisma logging

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -11,11 +11,12 @@ const connectionString = process.env.DATABASE_URL!;
 const adapter = new PrismaNeon({ connectionString });
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
 
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     adapter,
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
+    log: ['error']
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
